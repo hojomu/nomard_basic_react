de5672f4a0ab134286f741d6b7bb6f0c04f6a6f2
(hojomu) <(whdan45@gmail.com)>
2023-03-22 18:54:17 +0900

    230322_youtube_api

--- youtubeapi/src/App.js
+++ youtubeapi/src/App.js
@@ -1,14 +1,15 @@
 import { useState , useEffect } from "react";
 
 function App() {
+  
   const [loading , setLoading] = useState(true);
+
   const getVideos = async () => {
-    const videos = await (
-      await fetch(
+    const response = await fetch(
         `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&regionCode=kr&key=AIzaSyCKtokkvXUMxJKSe9r7zsuHvRQCAfHJlIc`
-      )
-    );
-    console.log(videos);
+      );
+    const data = await response.json();
+    console.log(data);
     setLoading(false);
   }
 
